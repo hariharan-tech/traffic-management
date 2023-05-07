fcb6b85dd7afdf1995eb0f6da9018b57710c64dc
Gurukishore-G <112895462+Gurukishore-G@users.noreply.github.com>
2023-05-08 00:23:23 +0530

    Add files via upload

---
+++ image_mail.py
@@ -0,0 +1,51 @@
+import smtplib
+# import imghdr
+# from email.message import EmailMessage
+
+# Sender_Email = ""
+# Reciever_Email = ""
+# # Password = input('Enter your email account password: ')
+
+# newMessage = EmailMessage()    #creating an object of EmailMessage class
+# newMessage['Subject'] = "Alert Email" #Defining email subject
+# newMessage['From'] = Sender_Email  #Defining sender email
+# newMessage['To'] = Reciever_Email  #Defining reciever email
+# newMessage.set_content('Hello, Please check!') #Defining email body
+
+# with open('img.jpeg', 'rb') as f:
+#     image_data = f.read()
+#     image_type = imghdr.what(f.name)
+#     image_name = f.name
+
+# newMessage.add_attachment(image_data, maintype='image', subtype=image_type, filename=image_name)
+
+# with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+#     smtp.login(Sender_Email,'app_pwd') #Login to SMTP server
+#     smtp.send_message(newMessage)      #Sending email using send_message method by passing EmailMessage object
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+
+msg = MIMEMultipart('alternative')
+msg['Subject'] = "SEND YES IF THIS IS WORKING FINE"
+msg['From'] = ''
+msg['To'] = ''
+
+text = MIMEText('<img src="cid:image1">', 'html')
+msg.attach(text)
+
+image = MIMEImage(open('img.jpg', 'rb').read())
+
+# Define the image's ID as referenced in the HTML body above
+image.add_header('Content-ID', '<image1>')
+msg.attach(image)
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login('','app_pwd') #Login to SMTP server
+    smtp.sendmail(msg['From'], msg['To'], msg.as_string())
+
+# s = smtplib.SMTP('localhost')
+# s.sendmail('', '', msg.as_string())
+# s.quit()
+
